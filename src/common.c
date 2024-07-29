3fbf9d36c612289d261ab5468c7460710ca17301
smaster4 <simon.marquardt@outlook.de>
2024-07-29 18:16:53 +0200

    feat: add parse_delay function

---
+++ src/common.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <threads.h>
+#include <time.h>
+
+#include "common.h"
+
+extern const time_t parse_delay(const char* delay_as_string) {
+  if(strlen(delay_as_string) > 30)
+    return 0;
+  time_t delay;
+  enum time_unit {hours, minutes, seconds} current_time_unit;
+
+  for(size_t i = 0; i < strlen(delay_as_string) && current_time_unit < seconds; i++) {
+    if(delay_as_string[i] == ':') {
+      current_time_unit += 1;
+      delay *= 60;
+      continue;
+    }
+    delay += (time_t) delay_as_string[i];
+  }
+
+  return delay;
+}
---
+++ src/common.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <time.h>
+
+// Parses hours:minutes:seconds into seconds
+extern const time_t parse_delay(const char* delayAsString);
