2598692b9c26cb57013ab856a27dc635b3283760
smaster4 <simon.marquardt@outlook.de>
2024-08-05 20:34:51 +0200

    feat: move config path functionality into common instead of gui

--- src/common.c
+++ src/common.c
@@ -1,7 +1,16 @@
 #include <string.h>
 #include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 #include "common.h"
+
+#ifdef __linux__
+static const char* _config_relative_path = "/.config/smaster4s-timer/";
+#endif
+#ifdef _WIN32
+static const char* _config_relative_path = "\\smaster4s-timer\\";
+#endif
 
 extern void parse_delay(const char* delay_as_string, int parsed_delay_buffer[3]) {
   for(size_t i = 0; i < 3; i++)
@@ -22,4 +31,26 @@ extern void parse_delay(const char* delay_as_string, int parsed_delay_buffer[3])
 
 extern const unsigned short int get_lenght_as_string(const unsigned int value) {
   return (value == 0 ? 1 : (int) (log10(value) + 1));
+}
+
+extern char* get_config_path(const char* additional_path) {
+  #ifdef __linux__
+  const char* home_path = getenv("HOME");
+  #endif
+  #ifdef _WIN32
+  const char* home_path = getenv("APPDATA");
+  #endif
+  if(home_path == NULL) {
+    #ifdef __linux__
+    fprintf(stderr, "You have no HOME enviorement variable. Please define HOME to be your home directory.");
+    #endif
+    #ifdef _WIN32
+    fprintf(stderr, "You have no APPDATA enviorement variable. Please define APPDATA to be your app data folder. (It should mostly be C:\\Users\\!YOUR_USER_NAME!\\AppData\\Roaming\\)"); // btw folder != directory
+    #endif
+    return NULL;
+  }
+  const size_t config_full_path_length = strlen(home_path) + strlen(_config_relative_path) + strlen(additional_path) + 1;
+  char* config_full_path = (char*) malloc(config_full_path_length);
+  snprintf(config_full_path, config_full_path_length, "%s%s%s", home_path, _config_relative_path, additional_path);
+  return config_full_path;
 }
--- src/common.h
+++ src/common.h
@@ -1,6 +1,12 @@
 #pragma once
 
 // Parses hours:minutes:seconds into seconds
+// beOF
 extern void parse_delay(const char* delay_as_string, int delay_buffer[3]);
 
+// 0FC
 extern const unsigned short int get_lenght_as_string(const unsigned int value);
+
+// Rc
+// rnOF
+extern char* get_config_path(const char* additional_config_path);
