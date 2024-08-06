003b4f797bf8f39d40f0a454c023de8184dfeb68
smaster4 <simon.marquardt@outlook.de>
2024-08-06 15:27:38 +0200

    feat: let the timer create a the layout file if it's missing

---
+++ src/file_flags.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define F_OK 0
+#define W_OK 2
+#define R_OK 4
--- src/main.c
+++ src/main.c
@@ -1,9 +1,21 @@
 #include <stdlib.h>
+#include <stdio.h>
+#ifdef __unix__
+#include <unistd.h>
+#endif
 
+#ifdef _WIN32
+#include <io.h>
+
+#include "file_flags.h"
+#endif
 #include "gui.h"
 #ifdef __linux__
 #include "cli.h"
 #endif
+#include "common.h"
+
+#include "../res/default_layout.h"
 
 int main(int argc, char** argv) {
   int status;
@@ -11,6 +23,23 @@ int main(int argc, char** argv) {
   if(getenv("DESKTOP_SESSION") == NULL)
     status = cli_run(argc, argv);
   #endif
+  char* config_path = get_config_path("main.ui");
+
+  if(
+    #ifdef __unix__
+    access(config_path, R_OK)
+    #endif
+    #ifdef _WIN32
+    _access(config_path, R_OK) // Yeah really its _access: https://learn.microsoft.com/en-us/cpp/c-runtime-library/reference/access-waccess
+    #endif
+    !=
+    0
+  ) {
+    FILE* file = fopen(config_path, "w");
+    fprintf(file, default_layout);
+    fclose(file);
+  }
+
   status = gui_run(argc, argv);
   return status;
 }
