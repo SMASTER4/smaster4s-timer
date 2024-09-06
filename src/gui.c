387995dfd432f4a57898c1bf464630122009d1c2
smaster4 <simon.marquardt@outlook.de>
2024-09-06 13:48:51 +0200

    fix: remove redundant error variable inside _load_css

--- src/gui.c
+++ src/gui.c
@@ -87,8 +87,6 @@ static void _load_css() {
   if(css_path == NULL)
     return;
 
-  GError** error;
-
   GtkCssProvider *provider = gtk_css_provider_new();
   gtk_css_provider_load_from_path(provider, css_path);
   gtk_style_context_add_provider_for_display(gdk_display_get_default(), GTK_STYLE_PROVIDER(provider), GTK_STYLE_PROVIDER_PRIORITY_USER);
